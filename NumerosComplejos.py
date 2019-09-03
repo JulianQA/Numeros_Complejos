710bc17df2a5040b179e2cb8c8a71b18d46161d4
JulianQA <54037880+JulianQA@users.noreply.github.com>
2019-09-03 14:55:57 -0500

    Update NumerosComplejos.py

--- NumerosComplejos.py
+++ NumerosComplejos.py
@@ -16,7 +16,7 @@ def resta(a,b):
 
 
 #Hacer la multiplicacion
-def multiplicacion(a,b):
+def multiplicacion(a,b):  
     first = a[0] * b[0]
     second = a[0] * b[1]
     third = a[1] * b[0]
@@ -24,7 +24,6 @@ def multiplicacion(a,b):
     ent = first-fourth
     imag = second+third
     return ent,imag
-
 #Hacer el conjugado
 def conju(a):
     res = (a[0],a[1]*-1)
@@ -92,37 +91,31 @@ def multi_escalar_vec(esc,v1):
 
 #Hacer suma de matrices
 def suma_matrices(m1,m2):
-    filas = len(m1)
-    for i in range(len(m1)):
-        x = len(m1[i])
-    cols = x
-    matriz = [[0]*cols for i in range(filas)]
-    for a in range(filas):
-        for b in range(cols):
-            matriz[a][b] = suma(m1[a][b],m2[a][b])
-    return matriz
+    matriz = [[0]*(len(m1[0])) for i in range(len(m1))]
+    if len(m1) == len(m2) and len(m1[0]) == len(m2[0]):
+        for a in range(len(m1)):
+            for b in range(len(m1[0])):
+                matriz[a][b] = suma(m1[a][b],m2[a][b])
+        return matriz
+    else:
+        return False
+
 #Hacer multiplicacion de martices por escalar
 def multi_matrices(esc,m1):
-    filas = len(m1)
-    for i in range(len(m1)):
-        x = len(m1[i])
-    cols = x
-    matriz_res = [[None]*cols for i in range(filas)]
-    for a in range(filas):
-        for b in range(cols):
-            matriz_res[a][b] = multiplicacion((esc,0),m1[a][b])
+    matriz_res = [[None]*len(m1[0]) for x in range(len(m1))]
+    if type(esc) is not tuple:
+        esc = (esc,0)
+    for a in range(len(m1)):
+        for b in range(len(m1[a])):
+            matriz_res[a][b] = multiplicacion(esc,m1[a][b])
     return matriz_res
 
 #Hacer Transpuesta
 def transpuesta(m1):
-    filas = len(m1)
-    for i in range(len(m1)):
-        x = len(m1[i])
-    cols = x
-    matriz_res = [[None]*cols for i in range(filas)]
+    matriz_res = [[None]*len(m1[0]) for i in range(len(m1))]
     
-    for i in range(filas):
-        for j in range(cols):
+    for i in range(len(m1)):
+        for j in range(len(m1[0])):
             matriz_res[i][j] = m1[j][i]
     return matriz_res
 
@@ -145,9 +138,13 @@ def mat_adjunta(m1):
 # Multipllicacion de  Matrices
 def multiplicacion_matrices(m1,m2):
     matriz_res = [[(0,0)]*len(m2[0]) for i in range(len(m1))]
+    cos = 0
+    if len(matriz_res) == 1:
+        cos = 1
+        
     for i in range(len(m1)):
         for j in range(len(m2[0])):
-            for k in range(len(matriz_res)):
+            for k in range(len(matriz_res)+cos):
                 matriz_res[i][j] = suma(matriz_res[i][j],multiplicacion(m1[i][k],m2[k][j]))
     return matriz_res
 
@@ -161,14 +158,12 @@ def unit(m1):
         return True
     else:
         return False
-
 #Revisar si es hermitian
 def hermitian(m1):
     if mat_adjunta(m1) == m1:
         return True
     else:
         return False
-    
 #Producto tensor entre vectores
 def producto_tensor_vector(v1,v2):
     res = []
@@ -176,7 +171,19 @@ def producto_tensor_vector(v1,v2):
         for j in range(len(v2)):
             res.append(multiplicacion(v1[i],v2[j]))
     return res
-#hacer la norma de matrices
+
+#Producto tensor entre matrices
+def producto_tensor_matriz(m1,m2):
+    res = []
+    for i in range(len(m1)):
+        for j in range(len(m1[i])):
+            res.append((multi_matrices(m1[i][j],m2)))
+    return res
+def accion(v,m):
+    res = multiplicacion_matrices(v,m)
+    return res
+
+#Hacer Norma
 def norma(m1):
     res = 0
     for i in range(len(m1)):
@@ -184,5 +191,13 @@ def norma(m1):
             res += (m1[i][j][0])**2 + (m1[i][j][1])**2
     raiz = round(res**0.5,2)
     return raiz
-
-
+#distancia entre matrices
+def distancia(m1,m2):
+    res = [[(0,0)]*len(m1[0]) for i in range(len(m1))]
+    for i in range(len(m1)):
+        for j in range(len(m1[0])):
+            res[i][j] = resta(m1[i][j],m2[i][j])
+    h = norma(res)
+    return h
+    
+    
